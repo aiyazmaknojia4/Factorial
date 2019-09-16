902d89a6c00f01aa636a9e5dcaf8ffd582e69818
an <alphateam.uh@gmail.com>
2019-09-15 21:22:45 -0400

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(3));
+            Console.ReadLine();
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
